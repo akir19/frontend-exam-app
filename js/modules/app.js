001da877cc8bc9afd73f1f2b9c14d4f4a0786673
Andrew Clarke <aigod2004@gmail.com>
2026-03-16 00:50:20 +0300

    feat: add ability to create cards

--- js/modules/app.js
+++ js/modules/app.js
@@ -1,15 +1,22 @@
 import { loadCards } from "./cards.js"
 import { renderCards } from "./ui.js"
+import { addCard } from "./ui.js"
 import { startExam } from "./exam.js"
 
 export async function initApp() {
 
   const cards = await loadCards()
 
-  renderCards(cards)
+  const updateUI = () => renderCards(cards)
+
+  updateUI()
 
   document
     .getElementById("examBtn")
     .addEventListener("click", () => startExam(cards))
+
+  document
+    .getElementById("addBtn")
+    .addEventListener("click", () => addCard(cards, updateUI))
 
 }
